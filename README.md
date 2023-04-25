73edca1f29fa19f4ceaf96a0a44f8abcec025fbb
Jason Watmore <cornflourblue@hotmail.com>
2023-04-25 14:58:32 +1000

    updated links

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# next-js-13-mongodb-registration-login-example
+# next-js-13-mysql-registration-login-example
 
-Next.js 13 + MongoDB - User Registration and Login Example
+Next.js 13 + MySQL - User Registration and Login Example
 
-Documentation at https://jasonwatmore.com/next-js-13-mongodb-user-registration-and-login-tutorial-with-example-app
+Documentation at https://jasonwatmore.com/next-js-13-mysql-user-registration-and-login-tutorial-with-example-app
--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,10 @@ function Document() {
                 {/* credits */}
                 <div className="text-center mt-4">
                     <p>
-                        <a href="https://jasonwatmore.com/next-js-13-mongodb-user-registration-and-login-tutorial-with-example-app" target="_top">Next.js 13 + MongoDB - User Registration and Login Tutorial with Example App</a>
+                        <a href="https://jasonwatmore.com/next-js-13-mysql-user-registration-and-login-tutorial-with-example-app" target="_blank">Next.js 13 + MySQL - User Registration and Login Tutorial with Example App</a>
                     </p>
                     <p>
-                        <a href="https://jasonwatmore.com" target="_top">JasonWatmore.com</a>
+                        <a href="https://jasonwatmore.com" target="_blank">JasonWatmore.com</a>
                     </p>
                 </div>
             </body>
